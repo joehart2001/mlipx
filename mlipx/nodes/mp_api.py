2c4ff4764618f381d2bcf0d630ce3da5c9d48c7c
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-11-19 19:37:55 +0100

    update mp api interface

--- mlipx/nodes/mp_api.py
+++ mlipx/nodes/mp_api.py
@@ -1,7 +1,9 @@
+import pathlib
+
+import ase.io
 import zntrack
 from mp_api import client
-
-# set MP_API_KEY environment variable
+from pymatgen.io.ase import AseAtomsAdaptor
 
 
 class MPRester(zntrack.Node):
@@ -14,13 +16,29 @@ class MPRester(zntrack.Node):
 
     Example
     -------
+    >>> os.environ["MP_API_KEY"] = "your_api_key"
     >>> MPRester(search_kwargs={"material_ids": ["mp-1234"]})
 
     """
 
     search_kwargs: dict = zntrack.params()
+    frames_path: pathlib.Path = zntrack.outs_path(zntrack.nwd / "frames.xyz")
 
     def run(self):
         with client.MPRester() as mpr:
-            docs = mpr.materials.summary.search(**self.search_kwargs)
-        print(docs)
+            docs = mpr.materials.search(**self.search_kwargs)
+
+        frames = []
+        adaptor = AseAtomsAdaptor()
+
+        for entry in docs:
+            structure = entry.structure
+            atoms = adaptor.get_atoms(structure)
+            frames.append(atoms)
+
+        ase.io.write(self.frames_path, frames)
+
+    @property
+    def frames(self) -> list[ase.Atoms]:
+        with self.state.fs.open(self.frames_path, mode="r") as f:
+            return list(ase.io.iread(f, format="extxyz"))
